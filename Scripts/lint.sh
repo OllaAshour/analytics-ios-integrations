260c4049ac62f33a7c0f72ca39914b27f10c300b
Olla Ashour <olla.ashour@andela.com>
2020-01-14 08:55:15 +0200

    DAND-43-Move formatting into a separate bash script

---
+++ Scripts/lint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Move to parent directory to format and lint
+cd ..
+
+# Run oclint related commands to check for any issues
+
+echo "$(tput setaf 4) Running OClint"
+
+unset LLVM_TARGET_TRIPLE_SUFFIX
+
+xcodebuild -target Segment-Facebook -configuration Debug -scheme Segment-Facebook clean
+xcodebuild -target Segment-Facebook -configuration Debug -scheme Segment-Facebook COMPILER_INDEX_STORE_ENABLE=NO | xcpretty -r json-compilation-database --output compile_commands.json
+oclint-json-compilation-database —e Segment_Facebook.h Segment_Facebook_vers.c Pods -- build -report-type xcode
+
+echo "$(tput setaf 4) Linting pod spec"
+
+# Move to integrations destinations to run pod lint
+cd Integrations/analytics-ios-integration-facebook-app-events
+
+# Run pod lint to check for any issues in podspec
+pod lib lint --use-libraries --allow-warnings
